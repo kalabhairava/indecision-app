91e08c26e3c708889615510b843077944613b8f0
Manjunatha D <manjunathad13@gmail.com>
2018-02-03 11:26:14 +0530

    Configure Babel and ESLint

--- src/app.js
+++ src/app.js
@@ -1,46 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const app = {
-  title: "Indecision App",
-  subtitle: "Put yourself in the hands of a computer",
-  options: ["One", "Two"]
+	title: 'Indecision App',
+	subtitle: 'Put yourself in the hands of a computer',
+	options: ['One', 'Two']
 };
 
 const appTemplate = (
-  <div>
-    <h1>{app.title ? app.title : "Anonymous App"}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    {getOptions()}
-    <ul>
-      <li>{app.options[0]}</li>
-      <li>{app.options[1]}</li>
-    </ul>
-  </div>
+	<div>
+		<h1>{app.title ? app.title : 'Indecision App'}</h1>
+		{app.subtitle && <p>{app.subtitle}</p>}
+		{getOptions()}
+		<ul>
+			<li>{app.options[0]}</li>
+			<li>{app.options[1]}</li>
+		</ul>
+	</div>
 );
 
 function getOptions(options) {
-  if (options && options.length > 0) return <p>Here are your options</p>;
+	if (options && options.length > 0) return <p>Here are your options</p>;
 }
 
 const user = {
-  name: "Manjunatha D",
-  age: 17,
-  location: "Bengaluru"
+	name: 'Manjunatha D',
+	age: 17,
+	location: 'Bengaluru'
 };
 
 const userTemplate = (
-  <div>
-    <h1>{user.name ? user.name : "Anonymous"}</h1>
-    {user.age >= 18 && <p>Age: {user.age}</p>}
-    {/* <p>Location: {getLocation(user.location)}</p> */}
-    {getLocation(user.location)}
-  </div>
+	<div>
+		<h1>{user.name ? user.name : 'Anonymous'}</h1>
+		{user.age >= 18 && <p>Age: {user.age}</p>}
+		{/* <p>Location: {getLocation(user.location)}</p> */}
+		{getLocation(user.location)}
+	</div>
 );
 
 // Conditional rendereing => call a function inside JSX expression, have all the conditional logic inside that function
 function getLocation(location) {
-  return location && <p>{user.location}</p>;
+	return location && <p>{user.location}</p>;
 }
 
-const appRoot = document.getElementById("app");
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(appTemplate, appRoot);
 
