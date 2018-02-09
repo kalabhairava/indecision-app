243f15593c24fd3cf75f7a76523e2e79492c13b5
Manjunatha D <manjunathad13@gmail.com>
2018-02-09 10:07:31 +0530

    Add yarn.lock file

--- src/app.js
+++ src/app.js
@@ -2,122 +2,125 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      options: [],
-      newOption: '',
-      choosenOption: ''
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+			options: []
+		};
 
-    this.onAddOption = this.onAddOption.bind(this);
-    this.onChooseWhatToDo = this.onChooseWhatToDo.bind(this);
-    this.onClearAll = this.onClearAll.bind(this);
-  }
+		this.onAddOption = this.onAddOption.bind(this);
+		this.onChooseWhatToDo = this.onChooseWhatToDo.bind(this);
+		this.onClearAll = this.onClearAll.bind(this);
+	}
 
-  render() {
-    return (
-      <div>
-        <Header title="Indecision App" subtitle="Put yourself in the hands of computer" />
-        <ActionButton
-          onChooseWhatToDo={this.onChooseWhatToDo}
-          hasOptions={this.state.options.length > 0}
-        />
-        <Options options={this.state.options} />
-        <AddOption
-          onAddOption={this.onAddOption}
-          newOption={this.state.newOption}
-          onAddOptionChange={this.onAddOptionChange}
-        />
-        <button onClick={this.onClearAll}> Clear all </button>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Header
+					title="Indecision App"
+					subtitle="Put yourself in the hands of computer"
+				/>
+				<ActionButton
+					onChooseWhatToDo={this.onChooseWhatToDo}
+					hasOptions={this.state.options.length > 0}
+				/>
+				<Options options={this.state.options} />
+				<AddOption onAddOption={this.onAddOption} />
+				<button onClick={this.onClearAll}> Clear all </button>
+			</div>
+		);
+	}
 
-  onAddOption(event) {
-    event.preventDefault();
+	onAddOption(option) {
+		const { options } = this.state;
 
-    let { options, newOption } = this.state;
-    newOption = newOption.trim();
+		if (!option) {
+			return 'Option cannot be empty';
+		} else if (options.includes(option)) {
+			return 'Option already exists. Please enter a new one.';
+		}
 
-    if (newOption) {
-      if (options.includes(newOption)) {
-        alert('Option already exists');
-        this.setState({ newOption: '' });
-      } else {
-        const updatedOptions = [...options, newOption];
-        this.setState({ options: updatedOptions, newOption: '' });
-      }
-    }
-  }
+		const updatedOptions = [...options, option];
+		this.setState({ options: updatedOptions });
+	}
 
-  onChooseWhatToDo() {
-    const { options } = this.state;
+	onChooseWhatToDo() {
+		const { options } = this.state;
 
-    if (options.length === 0) {
-      alert('Add some options');
-    } else {
-      const choosenOption = Math.floor(Math.random() * options.length);
-      alert(`You should do ${options[choosenOption]}`);
-    }
-  }
+		if (options.length === 0) {
+			alert('Add some options');
+		} else {
+			const choosenOption = Math.floor(Math.random() * options.length);
+			alert(`You should do ${options[choosenOption]}`);
+		}
+	}
 
-  onClearAll() {
-    this.setState({ options: [] });
-  }
+	onClearAll() {
+		this.setState({ options: [] });
+	}
 }
 
 function Header({ title, subtitle }) {
-  return (
-    <div>
-      <h1> {title} </h1>
-      <h3> {subtitle} </h3>
-    </div>
-  );
+	return (
+		<div>
+			<h1> {title} </h1>
+			<h3> {subtitle} </h3>
+		</div>
+	);
 }
 
 function ActionButton({ onChooseWhatToDo, hasOptions }) {
-  return (
-    <button onClick={onChooseWhatToDo} disabled={hasOptions}>
-      What should I do?
-    </button>
-  );
+	return (
+		<button onClick={onChooseWhatToDo} disabled={!hasOptions}>
+			What should I do?
+		</button>
+	);
 }
 
 function Options({ options }) {
-  return options.map((option) => <li key={option}> {option} </li>);
+	return options.map(option => <li key={option}> {option} </li>);
 }
 
 class AddOption extends React.Component {
-  constructor(props) {
-    const { onAddOption, newOption, onAddOptionChange } = props;
+	constructor(props) {
+		super(props);
+		this.state = {
+			option: '',
+			error: undefined
+		};
 
-    this.state = {
-      error: undefined
-    };
+		this.onAddOptionChange = this.onAddOptionChange.bind(this);
+		this.onAddOption = this.onAddOption.bind(this);
+	}
 
-    this.onAddOptionChange = this.onAddOptionChange.bind(this);
-  }
+	render() {
+		return (
+			<form onSubmit={this.onAddOption}>
+				<input
+					type="text"
+					value={this.state.option}
+					onChange={this.onAddOptionChange}
+					placeholder="Enter option"
+					name="option"
+				/>
+				<button type="submit"> Add </button>
+				{this.state.error && <p>{this.state.error}</p>}
+			</form>
+		);
+	}
 
-  render() {
-    return (
-      <form onSubmit={onAddOption}>
-        <input
-          type="text"
-          value={newOption}
-          onChange={onAddOptionChange}
-          placeholder="Enter option"
-          name="option"
-        />
-        <button type="submit"> Add </button>
-        {this.state.error && <p>{this.state.error}</p>}
-      </form>
-    );
-  }
+	onAddOptionChange(event) {
+		this.setState({ option: event.target.value });
+	}
 
-  onAddOptionChange(event) {
-    this.setState({ newOption: event.target.value });
-  }
+	onAddOption(event) {
+		event.preventDefault();
+
+		const option = event.target.elements.option.value.trim();
+		const error = this.props.onAddOption(option);
+
+		this.setState({ option: '', error });
+	}
 }
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('root'));
