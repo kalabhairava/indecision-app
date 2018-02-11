557619ec18ff191adeea33b1efc510d6c55e65b2
Manjunatha D <manjunathad13@gmail.com>
2018-02-11 10:45:26 +0530

    Configure webpack to compile and load SCSS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DecideForMeApp from './components/DecideForMeApp.js';
-import './styles/styles.css';
+import './styles/styles.scss';
 
 ReactDOM.render(<DecideForMeApp />, document.getElementById('root'));
